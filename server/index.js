632c5b25ef801bd984ce9bbbeedc92144a64111d
Calli Oberembt <croberembt@gmail.com>
2021-03-10 18:33:16 -0600

    got external css file imported into index.ejs

--- server/index.js
+++ server/index.js
@@ -3,6 +3,10 @@ const app = express();
 
 app.set('view engine', 'ejs');
 
+app.use(express.static('public')); 
+//app.use(express.static(__dirname + '/public'));
+//app.use('/public', express.static('public'));
+
 app.get('/', (req, res) => {
   res.render('index');
 });
