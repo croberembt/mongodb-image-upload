bb8a5e3795994332ea0bcbf661ceeb0feffd1c24
Calli Oberembt <croberembt@gmail.com>
2021-03-15 20:20:28 -0500

    continued building out app, brought in several modules

--- server/index.js
+++ server/index.js
@@ -1,16 +1,27 @@
 const express = require('express'); 
+const path = require('path');
+const crypto = require('crypto'); 
+const mongoose = require('mongoose'); 
+const GridFsStorage = require('multer-gridfs-storage');
+const Grid = require('gridfs-stream');
+const methodOverride = require('method-override'); 
+const dotenv = require('dotenv'); 
+
 const app = express(); 
 
+//middleware
+app.use(express.json());
+app.use(methodOverride('_method')); 
 app.set('view engine', 'ejs');
-
 app.use(express.static('public')); 
-//app.use(express.static(__dirname + '/public'));
-//app.use('/public', express.static('public'));
+
+dotenv.config();
+
+const mongoURI = process.env.MONGO_URI; 
 
 app.get('/', (req, res) => {
   res.render('index');
 });
 
-const port = 5000; 
-
+const port = process.env.PORT; 
 app.listen(port, () => console.log(`Server started on port ${port}`)); 
