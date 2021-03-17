9166bb84aaa980d770a82b39e9428045b70b3362
Calli Oberembt <croberembt@gmail.com>
2021-03-16 20:31:02 -0500

    worked on initializing and using grid fs

--- server/index.js
+++ server/index.js
@@ -1,27 +1,42 @@
 const express = require('express'); 
-const path = require('path');
 const crypto = require('crypto'); 
 const mongoose = require('mongoose'); 
 const GridFsStorage = require('multer-gridfs-storage');
 const Grid = require('gridfs-stream');
 const methodOverride = require('method-override'); 
-const dotenv = require('dotenv'); 
+
+const path = require('path');
+const { connect } = require('http2');
+require('dotenv').config({ path: path.resolve(__dirname, './.env') });
 
 const app = express(); 
 
-//middleware
+// create Mongo connectin
+const conn = mongoose.connect(process.env.DATABASE_URL, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+}); 
+
+// middleware
 app.use(express.json());
 app.use(methodOverride('_method')); 
 app.set('view engine', 'ejs');
 app.use(express.static('public')); 
 
-dotenv.config();
-
-const mongoURI = process.env.MONGO_URI; 
+// initialize gfs
+let gfs; 
+conn.once('open', () => {
+  // initialize stream
+  gfs = Grid(conn.db, mongoose.mongo); 
+  gfs.collection('images')
+}); 
 
 app.get('/', (req, res) => {
   res.render('index');
 });
 
 const port = process.env.PORT; 
-app.listen(port, () => console.log(`Server started on port ${port}`)); 
+
+app.listen(port, () => {
+  console.log(`Listening at http://localhost:${port}`)
+}); 
